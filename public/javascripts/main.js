1cafb0e40a41b249cf0156f74d5402cc4715e29a
Robert Ahlberg <rob.ahlberg@gmail.com>
2016-03-20 21:57:14 +0100

    Add search by hash url

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,40 +1,54 @@
 $(document).ready(function() {
+  var $urlInput = $('#url-input');
 
-  $('#url-input').focus();
-  $('#url-input').keypress(function(e) {
+  // Search by hash
+  var hashString = window.location.hash.substring(1);
+
+
+  if (hashString.length > 0) {
+    $urlInput.val(hashString);
+    sendData(hashString);
+  } else {
+    $urlInput.focus();
+  }
+
+  $urlInput.keypress(function(e) {
     if (e.keyCode === 13 && $('.overlay').is(':hidden')) {
       sendData($(this).blur().val());
+      window.location.hash = $(this).val();
     }
   });
 
   $('body').on('click', function() {
     $('.image-modal').fadeOut('fast');
-  })
+  });
+
+
+  function sendData(urlString) {
+    $('.overlay').fadeIn('fast');
+    $.post({
+      url: '/',
+      data: {webURL: urlString},
+      dataType: 'json',
+      timeout: 20000,
+      success: function(response) {
+        if (response.success) {
+          $('.image-modal img').prop('src', 'data:image/png;base64,' + response.image)
+          $('.image-modal').fadeIn('fast');
+        } else {
+          $urlInput.focus();
+          $('.middle-pane h1').removeClass('dangle').outerWidth($('.middle-pane h1').outerWidth());
+          $('.middle-pane h1').addClass('dangle');
+        }
+      },
+      error: function(jqXHR, error) {
+        console.error(error);
+        $urlInput.focus();
+      },
+      complete: function() {
+        $('.overlay').fadeOut('fast');
+      }
+    })
+  }
 
 });
-
-function sendData(urlString) {
-  $('.overlay').fadeIn('fast');
-  $.post({
-    url: '/',
-    data: {webURL: urlString},
-    dataType: 'json',
-    timeout: 20000,
-    success: function(response) {
-      if (response.success) {
-        $('.image-modal img').prop('src', 'data:image/png;base64,' + response.image)
-        $('.image-modal').fadeIn('slow');
-      } else {
-        $('.middle-pane h1').removeClass('dangle').outerWidth($('.middle-pane h1').outerWidth());
-        $('.middle-pane h1').addClass('dangle');
-      }
-    },
-    error: function(jqXHR, error) {
-      console.error(error);
-      $('#url-input').focus();
-    },
-    complete: function() {
-      $('.overlay').fadeOut('fast');
-    }
-  })
-}
